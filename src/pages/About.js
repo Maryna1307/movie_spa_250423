acea5b8cfe32d1ec1671d7290e03f8d83c0b956a
Your Name <you@example.com>
2023-09-07 21:25:08 +0300

    Preparation files for upload

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+function About() {
+    return (
+        <main className='container main'>
+            <h2>Maryna Kravchenko - front-end developer</h2>
+            <h3>Beetroot Academy - Ukraine</h3>
+        </main>
+    )
+}
+
+export { About }
